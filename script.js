98af966a59a004d0b510358603c9fe1b89dcd907
pineapple laptop <martinkosel@yahoo.de>
2023-10-10 14:12:54 +0100

    Adds nav bar styling and a responsive mobile nav bar with drop-down menu.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function toggleMenu() {
-    const menu = document.getElementById(".menu-links");
-    const icon = document.getElementById(".menu-icon");
+    const menu = document.querySelector(".mobile-menu-links");
+    const icon = document.querySelector(".mobile-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open")
-    
-
+    icon.classList.toggle("open");
 }
