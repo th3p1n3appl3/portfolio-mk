7f2cb7191fcf19b2eef45f06db62e1ea2504fac9
pineapple laptop <martinkosel@yahoo.de>
2023-10-10 13:24:10 +0100

    adds basic html boilerplate + both desktop and mobile nav bar
    Web design is not one of my strengths and I did not like the mock-up design. I hence looked out for a youtube video and found this one that I liked:  https://www.youtube.com/watch?v=ldwlOzRvYOU

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById(".menu-links");
+    const icon = document.getElementById(".menu-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+    
+
+}
